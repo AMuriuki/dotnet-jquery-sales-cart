7128d908b4273670c41e64ded7768d82956c845e
Arnold <arnoldnderitu@gmail.com>
2023-05-01 20:01:12 +0300

    modify models

--- Models/Sales.cs
+++ Models/Sales.cs
@@ -1,9 +1,22 @@
-public class Sales
+public class Product
 {
     public int Id { get; set; }
-    
-    public int ProductId { get; set; }
-    public Product Product { get; set; }
-
+    public string? Name { get; set; }
     public decimal Price { get; set; }
+    public string? sku { get; set; }
+    public ICollection<SaleProduct>? SaleProducts { get; set; } = new List<SaleProduct>();
+}
+public class Sale
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public ICollection<SaleProduct>? SaleProducts { get; set; }
+}
+
+public class SaleProduct
+{
+    public int SaleId { get; set; }
+    public Sale? Sale { get; set; }
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
 }
