126dabc6fb48addfc5a392cdc05cfc4964057073
Arnold <arnoldnderitu@gmail.com>
2023-05-01 20:01:20 +0300

    add data context

---
+++ Data/SalesContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace sales_invoicing_dotnet.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext(DbContextOptions<SalesContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleProduct> SaleProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SaleProduct>().HasKey(sp => new { sp.SaleId, sp.ProductId });
+
+            modelBuilder.Entity<SaleProduct>().HasOne(sp => sp.Sale).WithMany(s => s.SaleProducts).HasForeignKey(sp => sp.SaleId);
+
+            modelBuilder.Entity<SaleProduct>().HasOne(sp => sp.Product).WithMany(p => p.SaleProducts).HasForeignKey(sp => sp.ProductId);
+        }
+
+    }
+}
