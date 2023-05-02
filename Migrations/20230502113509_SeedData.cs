9de4a17c24e85e30727b032aba0ab933cc13c307
Arnold <arnoldnderitu@gmail.com>
2023-05-02 14:38:02 +0300

    seed database

---
+++ Migrations/20230502113509_SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace sales_invoicing_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price", "sku" },
+                values: new object[,]
+                {
+                    { 1, "Apple AirPods Pro", 8500.00m, "P1" },
+                    { 2, "Xiamo Redmi Buds", 2700.00m, "P10" },
+                    { 3, "Oraimo Wireless Earphone", 3400.00m, "P100" },
+                    { 4, "Netac USB Type-C 128 GB", 1600.00m, "P1000" },
+                    { 5, "Sandisk USB 32GB ", 8500.00m, "P10000" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
