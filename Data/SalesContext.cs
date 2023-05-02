0bc4f3878182d6932ee9fca882fe58b3175147cd
Arnold <arnoldnderitu@gmail.com>
2023-05-02 14:32:07 +0300

    create dummy data

--- Data/SalesContext.cs
+++ Data/SalesContext.cs
@@ -20,6 +20,14 @@ namespace sales_invoicing_dotnet.Data
             modelBuilder.Entity<SaleProduct>().HasOne(sp => sp.Sale).WithMany(s => s.SaleProducts).HasForeignKey(sp => sp.SaleId);
 
             modelBuilder.Entity<SaleProduct>().HasOne(sp => sp.Product).WithMany(p => p.SaleProducts).HasForeignKey(sp => sp.ProductId);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Apple AirPods Pro", Price = 8500.00M, sku = "P1" },
+                new Product { Id = 2, Name = "Xiamo Redmi Buds", Price = 2700.00M, sku = "P10" },
+                new Product { Id = 3, Name = "Oraimo Wireless Earphone", Price = 3400.00M, sku = "P100" },
+                new Product { Id = 4, Name = "Netac USB Type-C 128 GB", Price = 1600.00M, sku = "P1000" },
+                new Product { Id = 5, Name = "Sandisk USB 32GB ", Price = 8500.00M, sku = "P10000" }
+                );
         }
 
     }
